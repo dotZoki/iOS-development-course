050e3e402cad1f2937ff50e43718215851cb7800
dotZoki <zoran.zoki@gmail.com>
2015-10-20 23:29:35 +0200

    Currency converter app

---
+++ Lesson3/CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Zoran Lazic on 20/10/15.
+//  Copyright © 2015 Zoran Lazic. All rights reserved.
+//
+
+import UIKit
+
+class Currency {
+    
+    private var type: String
+    
+    init(curencyName: String) {
+        self.type = curencyName.uppercaseString
+    }
+    
+    func getType() -> String {
+        return self.type
+    }
+}
+
+enum CurrencyUnsuported: ErrorType {
+    case InvalidPair
+}
+
+class Convereter {
+    
+    static let exchangeRates: [String: Double] = [
+        "USD-EUR": 0.88,
+        "JPY-EUR": 0.0074,
+        "CHF-EUR": 0.92,
+    ]
+    
+    static func convert(value: Double, fromCurrency: Currency, toCurrency: Currency) throws -> (calculatedValue: Double, toCurrency: Currency, exchangeRate: String) {
+        
+        if let exchangeRate = Convereter.exchangeRates["\(fromCurrency.type)-\(toCurrency.type)"] {
+            return (value * exchangeRate, toCurrency, "\(fromCurrency.type)-\(toCurrency.type): \(exchangeRate)")
+        } else if let exchangeRate = Convereter.exchangeRates["\(toCurrency.type)-\(fromCurrency.type)"] {
+            return (value / exchangeRate, toCurrency, "\(toCurrency.type)-\(fromCurrency.type): \(exchangeRate)")
+        }
+        
+        throw CurrencyUnsuported.InvalidPair
+        
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currencyUsd: UIButton!
+    @IBOutlet weak var currencyJpy: UIButton!
+    @IBOutlet weak var conversionText: UILabel!
+    @IBOutlet weak var convertValue: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currencyUsd.selected = true
+        currencyJpy.selected = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func selectCurrencyUsd(sender: UIButton) {
+        sender.selected = true
+        currencyJpy.selected = false
+    }
+    
+    
+    @IBAction func selectCurrencyJpy(sender: UIButton) {
+        sender.selected = true
+        currencyUsd.selected = false
+    }
+    
+    @IBAction func converToEur(sender: UIButton) {
+        var currency: Currency
+        
+        switch true {
+        case currencyJpy.selected:
+            currency = Currency(curencyName: "JPY")
+        default:
+            currency = Currency(curencyName: "USD")
+        }
+        
+        if let cValue = Double(convertValue.text!) {
+            convertValue.text = String(format:"%.2f", cValue)
+            
+            do {
+                let conversionResult:(Double, Currency, String) = try Convereter.convert(cValue, fromCurrency: currency, toCurrency: Currency(curencyName: "EUR"))
+                let resultValue: String = String(format:"%.2f", conversionResult.0)
+                conversionText.text = "\(convertValue.text!) \(currency.getType()) is \(resultValue) EUR"
+            } catch CurrencyUnsuported.InvalidPair {
+                UIAlertView(title: "Wrong input", message: "Input is not OK", delegate: nil, cancelButtonTitle: "OK").show()
+            } catch {
+                UIAlertView(title: "?", message: "Something went wrong ...", delegate: nil, cancelButtonTitle: "OK").show()
+            }
+        } else {
+            UIAlertView(title: "?", message: "Did you input valid double?", delegate: nil, cancelButtonTitle: "OK").show()
+        }
+        
+        
+    }
+}
+
